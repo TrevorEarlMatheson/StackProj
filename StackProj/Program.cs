04a96ac0ba8be14f3a039e8b9d0fe156b02fa588
Trevor Matheson <trevor.earl.matheson@gmail.com>
2021-09-14 15:15:13 -0600

    Add project files.

---
+++ StackProj/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StackProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+
+            stack.Push(1);
+            stack.Push(.00000002);
+            stack.Push("Take that stack");
+            // stack.Push(null);
+            stack.Push(new int[8]);
+
+            stack.StackCount();
+
+            Console.WriteLine(stack.Pop());
+
+            Console.WriteLine(stack.Pop());
+
+            stack.StackCount();
+
+            stack.Clear();
+
+            stack.StackCount();
+        }
+    }
+}
---
+++ StackProj/Stack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+
+namespace StackProj
+{
+    class Stack
+    {
+        private ArrayList _Stack;
+
+        public Stack()
+        {
+            _Stack = new ArrayList();
+        }
+
+        public void Push(Object obj)
+        {
+            if(obj == null)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                _Stack.Add(obj);
+            }
+        }
+
+        public Object Pop()
+        {
+            if(_Stack.Count <= 0)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+            {
+                var obj = _Stack[_Stack.Count - 1];
+                _Stack.RemoveAt(_Stack.Count - 1);
+                return obj;
+            }
+        }
+
+        public void Clear()
+        {
+            _Stack.Clear();
+        }
+
+        public void StackCount()
+        {
+            Console.WriteLine("Current stack count: {0}", _Stack.Count);
+        }
+    }
+}
